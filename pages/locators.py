65bfae671a168bf23ddb9bb24e25687aad6fed44
AlekzzR <alekzzr@gmail.com>
2021-09-11 21:55:50 +0300

    Добавлены локаторы для выполнения 1 сценария

---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocate:
+    SEACH_FIELD = (By.ID, '#text')
+    SUGGEST_TABLE = (By.CSS_SELECTOR, '[role=listbox]')
+    SERVICES = (By.CSS_SELECTOR, '.services-new__item-title')
+    PICTIRE_LINK = (By.CSS_SELECTOR, '[data-statlog="services_new.item.images.2"]')
+    WORD_TO_SEACH = 'Тензор'
+
+class ResultSearchPage:
+    SEARCH_RESULT = (By.ID,'#search-result')
+
